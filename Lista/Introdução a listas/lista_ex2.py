c59f9ed21a69fd8a9f38d87d256101c1b763e6e8
VsPun <vspun92@gmail.com>
2019-10-01 08:04:42 +0300

    Update lista_ex2.py
    Corrected a typo

--- Lista/Introdução a listas/lista_ex2.py
+++ Lista/Introdução a listas/lista_ex2.py
@@ -2,11 +2,11 @@
 
 names = ['jake', 'gina', 'holt', 'boyle', 'amy', 'rosa', 'terry']
 
-print('O ' + names[0].title() + ' está aprendendo Pyton!')
-print('A ' + names[1].title() + ' está aprendendo Pyton!')
-print('O capitão ' + names[2].title() + ' está aprendendo Pyton!')
-print('O ' + names[3].title() + ' está aprendendo Pyton!')
-print('A ' + names[4].title() + ' está aprendendo Pyton!')
-print('A ' + names[5].title() + ' está aprendendo Pyton!')
-print('O ' + names[6].title() + ' está aprendendo Pyton!')
+print('O ' + names[0].title() + ' está aprendendo Python!')
+print('A ' + names[1].title() + ' está aprendendo Python!')
+print('O capitão ' + names[2].title() + ' está aprendendo Python!')
+print('O ' + names[3].title() + ' está aprendendo Python!')
+print('A ' + names[4].title() + ' está aprendendo Python!')
+print('A ' + names[5].title() + ' está aprendendo Python!')
+print('O ' + names[6].title() + ' está aprendendo Python!')
 
